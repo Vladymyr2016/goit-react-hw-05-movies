f237a091d8b1e1a565f13c4f9b32d107ab35c4d8
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-03-06 14:35:14 +0000

    Deploying to gh-pages from @ Vladymyr2016/goit-react-hw-05-movies@4b51e4730833f7f0cc1a3a4ee5e00c5834d3b7fc 🚀

---
+++ static/js/673.73aaba7c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[673],{673:function(e,r,s){s.r(r);var i=s(791),l=s(87),n=s(689),c=s(184);r.default=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)("header",{children:(0,c.jsxs)("ul",{children:[(0,c.jsx)("li",{children:(0,c.jsx)(l.rU,{to:"/",children:"Home"})}),(0,c.jsx)("li",{children:(0,c.jsx)(l.rU,{to:"/movies",children:"Movies"})})]})}),(0,c.jsx)(i.Suspense,{children:(0,c.jsx)(n.j3,{})})]})}}}]);
+//# sourceMappingURL=673.73aaba7c.chunk.js.map
