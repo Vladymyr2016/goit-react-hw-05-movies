2bd33d93cd637b928d98e13a9b4942b5fa913ff8
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-03-06 15:08:39 +0100

    lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './Home/Home';
-import Movies from './Movies/Movies';
-import NotFound from './NotFound/NotFound';
-import Layout from './Layout/Layout';
-import MovieDetails from './MovieDetails/MovieDetails';
-import Cast from './Cast/Cast';
-import Reviews from './Reviews/Reviews';
+// import Home from './Home/Home';
+// import Movies from './Movies/Movies';
+// import NotFound from './NotFound/NotFound';
+// import Layout from './Layout/Layout';
+// import MovieDetails from './MovieDetails/MovieDetails';
+// import Cast from './Cast/Cast';
+// import Reviews from './Reviews/Reviews';
+
+const Home = lazy(() => import('./Home/Home'));
+const Movies = lazy(() => import('./NotFound/NotFound'));
+const NotFound = lazy(() => import('./NotFound/NotFound'));
+const Layout = lazy(() => import('./Layout/Layout'));
+const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { FeatchReviews } from 'services/GetTrandingMovie';
 
 const Reviews = ({ movieId }) => {
-  const [movie, setMovie] = useState([]);
+  const [movies, setMovie] = useState([]);
   useEffect(() => {
     FeatchReviews(movieId)
       .then(data => setMovie(data))
       .catch(error => console.log(error));
   }, [movieId]);
+  console.log(movies);
   return (
     <div>
-      <li>
-        <p>{movie.title}</p>
-      </li>
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.id}>
+            <p>{movie.title}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
