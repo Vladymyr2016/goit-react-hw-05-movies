d57bae271d69d21912dff3b6efff12dfa7ffc486
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-05-01 09:41:55 +0200

    css 2

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,16 @@
 import React, { Suspense } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import s from './Layout.module.css';
 
 const Layout = () => {
   return (
     <div>
       <header>
-        <ul>
-          <li>
+        <ul className={s.items}>
+          <li className={s.item}>
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className={s.item}>
             <Link to="/movies">Movies</Link>
           </li>
         </ul>
